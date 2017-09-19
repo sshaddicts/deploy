85791335c2a47812aa9aeaf4398ca7d175ec0b0a
Andrii Bakal <andrew.bakal@gmail.com>
2017-09-20 01:46:58 +0300

    gelf

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
-docker-compose pull
-docker-compose up -d --force-recreate
+docker-compose stop crossbar
+docker-compose stop neuralswarm
+docker-compose rm -f crossbar
+docker-compose rm -f neuralswarm
 
-curl -X POST https://api.telegram.org/bot425164421:AAF28SLXHQbPJkQ2MJs8CeiSwYbRajWvd_Q/sendMessage -H 'content-type: application/x-www-form-urlencoded' -d 'chat_id=-1001143086398' -d 'text=Deploy on production is completed. 😎'
+function notify {
+    curl -X POST https://api.telegram.org/bot425164421:AAF28SLXHQbPJkQ2MJs8CeiSwYbRajWvd_Q/sendMessage -H 'content-type: application/x-www-form-urlencoded' -d 'chat_id=-1001143086398' -d 'text=Deploy on production is completed. 😎'
+}
+
+if [[ $HOSTNAME =~ 'vps' ]]
+    then
+        docker-compose pull
+        notify;
+fi
+
+docker-compose up -d;
